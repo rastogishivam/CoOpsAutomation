487a618ed0ec1d29c7b2b1c8c6e42d489077351b
Monika S <monika.srivastava@aidenai.com>
2024-05-14 11:45:52 +0530

    Test

--- CoOpsEvent/src/test/java/com/event/e2e/Event_EndToEndScenarios.java
+++ CoOpsEvent/src/test/java/com/event/e2e/Event_EndToEndScenarios.java
@@ -7,6 +7,6 @@ import org.testng.annotations.Test;
 public class Event_EndToEndScenarios extends BaseTest {
     @Test(description = "Validate to Purchase the policy with 1 Vehicle and 1 Driver", priority = 1, dataProvider = "fordUI_Web_TestData", dataProviderClass = DataProviderUtils.class)
     public void validate_Ford_E2EScenario_1Driver_1Vehicle(String vinNumber, String homeAddress, String yearlyKM, String driverFirstName, String driverLastName, String sex, String marital, String dob, String driverLicClass, String ageForGLic, String email, String phoneNum, String driverLic, String addInfo){
-        System.out.println("Event Test Class");
+        System.out.println("Event Test Class.");
     }
 }
